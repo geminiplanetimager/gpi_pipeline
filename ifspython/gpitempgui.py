af7798e4f715f1c92ddd5221b785c5759ce97b35
Marshall Perrin <mperrin@stsci.edu>
2010-07-27 02:02:45 +0000

    Major update to version 2.0

--- ifspython/gpitempgui.py
+++ ifspython/gpitempgui.py
@@ -35,7 +35,7 @@ class GPIMechanism(object):
         self.keyword=keyword
 
         self.frame = ttk.Frame(root, padx=2, pady=2) #padding=(10,10,10,10)) #, padx=10, pady=10)
-        ttk.Label(self.frame,text=" "+self.name+":    ", **formatting).grid(row=0,column=0)
+        ttk.Label(self.frame,text=" "+self.name+": ", **formatting).grid(row=0,column=0)
 
         self.value=' -Unknown- '
         if positions is None:
@@ -58,6 +58,10 @@ class GPIMechanism(object):
         self.pos=ttk.Label(self.frame,text=self.value, **formatting)
         self.pos.grid(row=0,column=3)
 
+
+        self.frame.grid_columnconfigure(0, minsize=100)
+        self.frame.grid_columnconfigure(1, minsize=180)
+ 
         self.parent=parent
 
     def move(self):
@@ -65,6 +69,7 @@ class GPIMechanism(object):
             sel = self.list.curselection()
             if len(sel) == 0:
                 self.parent.log( "Nothing selected!")
+                return
             else:
                 val = self.pos_names[int(self.list.curselection()[0])]
 
@@ -99,18 +104,22 @@ class GPI_TempGUI(object):
 
         self.oldfitsset=set([])
 
-        self.IFSController = gpiifs.IFSController(parent=self)
 
-        self.datadir = self.IFSController.datadir 
+        self.datadir = gpiifs.getDataDir() 
+        #self.datadir = self.IFSController.datadir 
         os.chdir(self.datadir)
 
         self.file_logger=None
         self.logger = ViewLog(self.root, geometry='550x600+650+50',logpath=self.datadir)
 
+
         self.log('Starting the GPI IFS Temporary GUI')
         self.log('Informative log messages will appear in this window.')
         self.log("    Log written to file "+self.logger.filename)
         self.log(' ')
+
+
+        self.IFSController = gpiifs.IFSController(parent=self)
 
         self._set_initial_filename()
         self.watchid=None
@@ -120,7 +129,6 @@ class GPI_TempGUI(object):
         self.root.update()
 
     def _make_widgets(self):
-
         self.root = Tkinter.Tk()
         self.root.title("GPITempGUI")
         self.root.geometry('+100+50')
@@ -160,62 +168,56 @@ class GPI_TempGUI(object):
         mframe.grid(row=r,stick=E+W)
         r=r+1
         #----
-        mframe = ttk.LabelFrame(frame, text="Header Keywords:", padx=2, pady=2, bd=2, relief=Tkinter.GROOVE) #padding=(10,10,10,10)) #, padx=10, pady=10)
+        mframe = ttk.LabelFrame(frame, text="Header Keywords:", padx=2, pady=2, bd=2, relief=Tkinter.GROOVE) 
 
         mr=0
-        ttk.Label(mframe,text="TARGET:",  **formatting).grid(row=mr,column=1, stick=N+E+S+W)
+        ttk.Label(mframe,text="TARGET:",  **formatting).grid(row=mr,column=0, stick=W)
         self.entry_target = ttk.Entry(mframe, )
-        self.entry_target.grid(row=mr,column=2,stick=N+E+S+W)
+        self.entry_target.grid(row=mr,column=1,stick=N+E+S+W)
         mr=mr+1
 
-        ttk.Label(mframe,text="COMMENTS:",  **formatting).grid(row=mr,column=1, stick=N+E+S+W)
+        ttk.Label(mframe,text="COMMENTS:",  **formatting).grid(row=mr,column=0, stick=W)
         self.entry_comment = ttk.Entry(mframe, )
-        self.entry_comment.grid(row=mr,column=2,stick=N+E+S+W)
+        self.entry_comment.grid(row=mr,column=1,stick=N+E+S+W)
         mr=mr+1
 
-        ttk.Label(mframe,text="OBSERVER:",  **formatting).grid(row=mr,column=1, stick=N+E+S+W)
+        ttk.Label(mframe,text="OBSERVER:",  **formatting).grid(row=mr,column=0, stick=W)
         self.entry_obs= ttk.Entry(mframe, )
-        self.entry_obs.grid(row=mr,column=2,stick=N+E+S+W)
+        self.entry_obs.grid(row=mr,column=1,stick=N+E+S+W)
+        ttk.Label(mframe,text="     ",  **formatting).grid(row=mr,column=2, stick=N+E+S+W)
         mr=mr+1
 
+        mframe.grid_columnconfigure(0, minsize=150)
+        mframe.grid_columnconfigure(1, minsize=150)
         mframe.grid(row=r,stick=E+W) 
         r=r+1
         #----
 
-        mframe = ttk.LabelFrame(frame, text="Actions:", padx=2, pady=2, bd=2, relief=Tkinter.GROOVE) #padding=(10,10,10,10)) #, padx=10, pady=10)
+        mframe = ttk.LabelFrame(frame, text="Hawaii 2RG:", padx=2, pady=2, bd=2, relief=Tkinter.GROOVE) #padding=(10,10,10,10)) #, padx=10, pady=10)
 
         mr=0
         ttk.Label(mframe, justify=Tkinter.LEFT, text=" Modes: 1=single, 2=CDS, 3=MCDS, 4=UTR ", 
               **formatting).grid(row=mr, columnspan=3, stick=W+E)
         mr=mr+1
 
-        ttk.Label(mframe,text="Mode nreads ncoadds:",  **formatting).grid(row=mr,column=1, stick=W)
+        ttk.Label(mframe,text="Mode nreads ncoadds:",  **formatting).grid(row=mr,column=0, stick=W)
         self.entry_mode = ttk.Entry(mframe,  width=10, )
-        self.entry_mode.grid(row=mr,column=2,stick=N+E+S+W)
+        self.entry_mode.grid(row=mr,column=1,stick=N+E+S+W)
         self.entry_mode.insert(0,"2 2 1")
         mr=mr+1
 
-        ttk.Label(mframe,text="ITIME:",  **formatting).grid(row=mr,column=1, stick=W)
+        ttk.Label(mframe,text="ITIME: ",  **formatting).grid(row=mr,column=0, stick=W)
         self.entry_itime = ttk.Entry(mframe,  width=10, )
-        self.entry_itime.grid(row=mr,column=2,stick=N+E+S+W)
+        self.entry_itime.grid(row=mr,column=1,stick=N+E+S+W)
         self.entry_itime.insert(0,"1.5")
+        ttk.Label(mframe,text="s",  **formatting).grid(row=mr,column=2, stick=N+E+S+W)
         mr=mr+1
 
-        ttk.Label(mframe,text=" ",  **formatting).grid(row=mr,column=1, stick=W)
-        mr=mr+1
-
-        ttk.Label(mframe,text="dir on Y:\\\\",  **formatting).grid(row=mr,column=1, stick=W)
-        self.entry_dir = ttk.Entry(mframe,  width=10, )
-        self.entry_dir.grid(row=mr,column=2,stick=N+E+S+W)
-        today = datetime.date.today()
-        self.entry_dir.insert(0,"%02d%02d%02d" % (today.year%100, today.month,today.day))
-        mr=mr+1
-
-        ttk.Label(mframe,text="filename:",  **formatting).grid(row=mr,column=1, stick=W)
+        ttk.Label(mframe,text="Filename:",  **formatting).grid(row=mr,column=0, stick=W)
         self.entry_fn = ttk.Entry(mframe,  width=10, )
-        self.entry_fn.grid(row=mr,column=2,stick=N+E+S+W)
+        self.entry_fn.grid(row=mr,column=1,stick=N+E+S+W)
         self.entry_fn.insert(0,'test0001')
-        ttk.Label(mframe,text=".fits",  **formatting).grid(row=mr,column=3, stick=N+E+S+W)
+        ttk.Label(mframe,text=".fits",  **formatting).grid(row=mr,column=2, stick=N+E+S+W)
         mr=mr+1
  
         buttonbar = ttk.Frame(mframe, **formatting)
@@ -223,13 +225,46 @@ class GPI_TempGUI(object):
         ttk.Button(buttonbar, text="Initialize", command=self.startSW, **formatting).grid(row=0,column=0)
         ttk.Button(buttonbar, text="Configure", command=self.configDet, **formatting).grid(row=0,column=1)
         ttk.Button(buttonbar, text="Take Image", command=self.takeImage, **formatting).grid(row=0,column=2)
-        ttk.Button(buttonbar, text="Print Keywords", command=self.printKeywords, **formatting).grid(row=0,column=3)
+        #ttk.Button(buttonbar, text="Print Keywords", command=self.printKeywords, **formatting).grid(row=0,column=3)
         ttk.Button(buttonbar, text="Abort", command=self.doAbort, **formatting).grid(row=0,column=4)
-        ttk.Button(buttonbar, text="Quit", command=self.quit, **formatting).grid(row=0,column=5)
+        #ttk.Button(buttonbar, text="Quit", command=self.quit, **formatting).grid(row=0,column=5)
 
+
+        mframe.grid_columnconfigure(0, minsize=150)
+        mframe.grid_columnconfigure(1, minsize=150)
         mframe.grid(row=r,stick=E+W) 
         r=r+1
 
+        #----
+        mframe = ttk.LabelFrame(frame, text="PupilCam:", padx=2, pady=2, bd=2, relief=Tkinter.GROOVE) #padding=(10,10,10,10)) #, padx=10, pady=10)
+
+        mr=0
+        ttk.Label(mframe,text="ITIME:",  **formatting).grid(row=mr,column=0, stick=W)
+        self.entry_pupilitime = ttk.Entry(mframe,  width=10, )
+        self.entry_pupilitime.grid(row=mr,column=1,stick=N+E+S+W)
+        self.entry_pupilitime.insert(0,"1.0")
+        ttk.Label(mframe,text="s",  **formatting).grid(row=mr,column=2, stick=N+E+S+W)
+        mr=mr+1
+
+        ttk.Label(mframe,text="Filename:",  **formatting).grid(row=mr,column=0, stick=W)
+        self.entry_pupilfn = ttk.Entry(mframe,  width=10, )
+        self.entry_pupilfn.grid(row=mr,column=1,stick=N+E+S+W)
+        self.entry_pupilfn.insert(0,'pupil0001')
+        ttk.Label(mframe,text=".fits",  **formatting).grid(row=mr,column=2, stick=N+E+S+W)
+        mr=mr+1
+ 
+        buttonbar = ttk.Frame(mframe, **formatting)
+        buttonbar.grid(row=mr,column=0, columnspan=3)
+        #ttk.Button(buttonbar, text="Initialize", command=self.startSW, **formatting).grid(row=0,column=0)
+        ttk.Button(buttonbar, text="Configure", command=self.configPupil, **formatting).grid(row=0,column=1)
+        ttk.Button(buttonbar, text="Take Image", command=self.takePupil, **formatting).grid(row=0,column=2)
+
+        mframe.grid_columnconfigure(0, minsize=150)
+        mframe.grid_columnconfigure(1, minsize=150)
+        mframe.grid(row=r,stick=E+W) 
+        r=r+1
+
+ 
         #----
 
         frame.grid(row=0) 
@@ -238,18 +273,16 @@ class GPI_TempGUI(object):
 
     def mainloop(self):
         self.root.mainloop()
-    
+
     def quit(self):
         if  tkMessageBox.askokcancel(title="Confirm Quit",message="Are you sure you want to quit?"):
             self.log(' User quit GPITempGUI.py ')
             self.log(' ' )
 
-
             if self.watchid is not None:
                 self.root.after_cancel(self.watchid)
             del self.logger
             self.root.destroy()
-
 
     def startSW(self):
         if  tkMessageBox.askokcancel(title="Confirm Start & Init",message="Are you sure you want to start the servers and initialize the hardware?"):
@@ -263,31 +296,56 @@ class GPI_TempGUI(object):
         parts = self.entry_mode.get().split()
         self.IFSController.configureDetector( float(self.entry_itime.get()), int(parts[0]), int(parts[1]), int(parts[2]) )
 
-    def takeImage(self):
-        self.IFSController.takeExposure(self.entry_fn.get() )
+    def configPupil(self):
+        self.pupil_exptime = float(self.entry_pupilitime.get())
+        self.log("Pupil exposure time set to %d s" % self.pupil_exptime)
 
+    def _incr_filename(self, widget, pupil=False):
         #increment the filename:
-        m =  re.search('(.*?)([0-9]+)',self.entry_fn.get())
+        m =  re.search('(.*?)([0-9]+)',widget.get())
         if m is not None:
             formatstr = "%0"+str(len(m.group(2)))+"d"
             newfn = m.group(1) + formatstr % ( int(m.group(2))+1)
-            self.entry_fn.delete(0,Tkinter.END)
-            self.entry_fn.insert(0,newfn)
+            widget.delete(0,Tkinter.END)
+            widget.insert(0,newfn)
 
-    def checkGMB(self):
-        val = self.IFSController.checkGMB_camReady()
-        self.log("    VALUE = %s" % string(val)) 
-
-
-    def printKeywords(self):
-        keywords = ['TARGET','COMMENTS','OBSERVER']
-        entries = [self.entry_target, self.entry_comment, self.entry_obs]
-        self.log('-- keywords --')
-        for k, e in zip(keywords, entries):
-            self.log("%8s = '%s'" % (k, e.get()))
+    def _store_keywords(self):
+        """ Store keyword values to controller object """
+        keywords = ['TARGET','COMMENTS','OBSERVER', 'REQ_DET', 'REQ_PUP']
+        entries = [self.entry_target, self.entry_comment, self.entry_obs, self.entry_mode, self.entry_pupilitime]
+        comm = ['What are we looking at?', 'Misc comments', 'Who?', 'Requested detector config in gpitempgui', 'Requested pupil config in gpitempgui']
+        for k, e, c in zip(keywords, entries, comm):
+            self.IFSController.updateKeyword(k, e.get(), c)
         for m in self.motors:
-            m.printkey()
-        self.log(' ')
+            self.IFSController.updateKeyword(*m.updatekey() , comment="Commanded motor position")
+            #f[0].header.update(*m.updatekey() )
+ 
+
+    def takePupil(self):
+        if not hasattr(self, "pupil_exptime"):
+            self.configPupil()
+        self._store_keywords()
+        self.IFSController.pupilExposure(self.entry_pupilfn.get(), time=self.pupil_exptime )
+        self._incr_filename(self.entry_pupilfn)
+
+    def takeImage(self):
+        self._store_keywords()
+        self.IFSController.takeExposure(self.entry_fn.get() )
+        self._incr_filename(self.entry_fn)
+
+    #def checkGMB(self):
+        #val = self.IFSController.checkGMB_camReady()
+        #self.log("    VALUE = %s" % string(val)) 
+
+    #def printKeywords(self):
+        #keywords = ['TARGET','COMMENTS','OBSERVER']
+        #entries = [self.entry_target, self.entry_comment, self.entry_obs]
+        #self.log('-- keywords --')
+        #for k, e in zip(keywords, entries):
+            #self.log("%8s = '%s'" % (k, e.get()))
+        #for m in self.motors:
+            #m.printkey()
+        #self.log(' ')
 
     def runcmd(self,cmdstring, **kwargs):
         self.log(">    " +cmdstring)
@@ -295,67 +353,67 @@ class GPI_TempGUI(object):
         result = subprocess.call(cmdstring, shell=True, **kwargs)
         self.log("     return code= %d " %result)
 
-
     def watchDir(self,init=False):
         self.watchid = self.root.after(1000,self.watchDir)
 
         if self.IFSController.is_exposing(): 
             self.log(' still exposing - waiting.')
             return
+        #self.log('watching')
+        self.IFSController.checkNewFiles()
 
-        directory = os.curdir
-        curfitsset = set(glob.glob(directory+os.sep+"*.fits"))
-        if init:
-            self.log("Now watching directory %s for new FITS files." % (self.datadir))
-            self.oldfitsset = curfitsset
-            #self.log("Ignoring already present FITS files" +' '.join(curfitsset))
-            self.log("Ignoring %d already present FITS files." % len(curfitsset))
-            return
-
-
-        newfiles = curfitsset - self.oldfitsset
-        if len(newfiles) > 0:
-            self.log("New FITS files!: "+ ' '.join(newfiles))
-            for fn in newfiles: 
-                if self.updateKeywords(fn):
-                    self.oldfitsset.add(fn)
-
-        #self.oldfitsset=curfitsset
-
-
-    def updateKeywords(self, filename):
-        try:
-        #if 1:
-            # need to watch out for network latency/long compute times meaning that
-            # the files are not fully written yet. In that case, wait for the write to finish.
-            # FIXME - this code could be more robust, to avoid infinite loop potential etc
-            size = os.path.getsize(filename)
-            while ((size != 8392320 ) & (size != 16784640)):
-                self.log('file %s not fully written to disk yet - waiting.' % filename)
-                return False
-                #for i in range(5):
-                    #self.root.update_idletasks() # process events!
-                    #time.sleep(0.1)
-                #size = os.path.getsize(filename)
-
-            f = pyfits.open(filename,mode='update')
-            if 'TARGET' not in f[0].header.keys():  # check if it's already been updated
-                keywords = ['TARGET','COMMENTS','OBSERVER']
-                entries = [self.entry_target, self.entry_comment, self.entry_obs]
-                f[0].header.add_history(' Header keywords updated by GPITempGUI.py')
-                for k, e in zip(keywords, entries):
-                    f[0].header.update(k,e.get())
-                
-                for m in self.motors:
-                    f[0].header.update(*m.updatekey() )
-                self.log("Updated FITS keywords in %s" % filename) 
-            f.close()
-            return True # either we updated it or it was already updated
-     
-
-        except:
-            self.log("ERROR: could not update FITS keywords in %s" % filename)
-            return False
+#        directory = os.curdir
+#        curfitsset = set(glob.glob(directory+os.sep+"*.fits"))
+#        if init:
+#            self.log("Now watching directory %s for new FITS files." % (self.datadir))
+#            self.oldfitsset = curfitsset
+#            #self.log("Ignoring already present FITS files" +' '.join(curfitsset))
+#            self.log("Ignoring %d already present FITS files." % len(curfitsset))
+#            return
+#
+#
+#        newfiles = curfitsset - self.oldfitsset
+#        if len(newfiles) > 0:
+#            self.log("New FITS files!: "+ ' '.join(newfiles))
+#            for fn in newfiles: 
+#                if self.updateKeywords(fn):
+#                    self.oldfitsset.add(fn)
+#
+#        #self.oldfitsset=curfitsset
+#
+#    def updateKeywords(self, filename):
+#        try:
+#        #if 1:
+#            # need to watch out for network latency/long compute times meaning that
+#            # the files are not fully written yet. In that case, wait for the write to finish.
+#            # FIXME - this code could be more robust, to avoid infinite loop potential etc
+#            size = os.path.getsize(filename)
+#            while ((size != 8392320 ) & (size != 16784640)):
+#                self.log('file %s not fully written to disk yet - waiting.' % filename)
+#                return False
+#                #for i in range(5):
+#                    #self.root.update_idletasks() # process events!
+#                    #time.sleep(0.1)
+#                #size = os.path.getsize(filename)
+#
+#            f = pyfits.open(filename,mode='update')
+#            if 'TARGET' not in f[0].header.keys():  # check if it's already been updated
+#                keywords = ['TARGET','COMMENTS','OBSERVER']
+#                entries = [self.entry_target, self.entry_comment, self.entry_obs]
+#                f[0].header.add_history(' Header keywords updated by GPITempGUI.py')
+#                for k, e in zip(keywords, entries):
+#                    f[0].header.update(k,e.get())
+#                
+#                for m in self.motors:
+#                    f[0].header.update(*m.updatekey() )
+#                self.log("Updated FITS keywords in %s" % filename) 
+#            f.close()
+#            return True # either we updated it or it was already updated
+#     
+#
+#        except:
+#            self.log("ERROR: could not update FITS keywords in %s" % filename)
+#            return False
 
     def _set_initial_filename(self):
         i=1
@@ -367,12 +425,11 @@ class GPI_TempGUI(object):
         self.entry_fn.insert(0,fn)
         self.log("Next filename set to %s.fits"% fn)
 
-
     def log(self, message, **kwargs):
         if self.logger is not None:
             self.logger.log(message, **kwargs)
 
-        
+
 
 #######################################################################     
 
