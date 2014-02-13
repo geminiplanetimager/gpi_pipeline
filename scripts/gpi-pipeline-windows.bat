76d7663c041f130f851e2840734aaf5c4d526bcd
Jason Wang <jwang@astro.berkeley.edu>
2014-02-13 01:10:10 +0000

    fixed bug in gpi-pipeline-compiled-windows.bat file. Updated colors on batch scripts to make it more readable.

--- scripts/gpi-pipeline-windows.bat
+++ scripts/gpi-pipeline-windows.bat
@@ -3,7 +3,7 @@
 ::
 :: Color is purely cosmetic to attempt to match *nix termnal colors
 
-start cmd /c "color 16 && idl %GPI_DRP_DIR%/scripts/gpi-pipeline__launchguis.pro"
+start cmd /c "color 3e && idl %GPI_DRP_DIR%/scripts/gpi-pipeline__launchguis.pro"
 
 :: Pause for a second just like the other script
 timeout/t 1
